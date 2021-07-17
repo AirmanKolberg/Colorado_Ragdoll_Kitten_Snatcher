8c2bd346b92da0dcb81cfda74cfc202b092c5c28
T.J. Kolberg <tkolberg@asu.edu>
2021-07-17 15:21:28 -0600

    😎 Example of a secrets.py File

--- secrets_example.py
+++ secrets_example.py
@@ -1,3 +1,13 @@
+sid = 'your twilio info'
+token = 'all goes here'
+twilio_number = '+12345678901'
+my_number = 'your number, with the +{country_code}'
+my_other_number = 'add another if you would like'
+
+ragdoll_link = 'https://www.mysticragdolls.com/available-ragdoll-kittens/'                          # Example link
+text_to_find = 'All kittens have been reserved, check back in about a week for the next litter.'    # Example message
+
+
 if __name__ == '__main__':
     
     pass
