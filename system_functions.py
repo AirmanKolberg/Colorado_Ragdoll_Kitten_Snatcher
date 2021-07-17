139bcbc76ae886d5478840bcc39a71ed4295ddc9
T.J. Kolberg <tkolberg@asu.edu>
2021-07-17 15:17:46 -0600

    ⚙ General System Functions

---
+++ system_functions.py
@@ -0,0 +1,46 @@
+from os import system
+from time import sleep
+
+
+def bash_command(user_in):
+    
+    _ = system(user_in)
+
+
+
+def clear_screen():
+
+    bash_command('clear')
+
+
+def countdown(seconds):
+    
+    # Count down until 0 so that the last second is counted
+    while seconds > -1:
+
+        mins, secs = divmod(seconds, 60)
+
+        # Setup grammar conditions for sec/secs
+        if secs != 1:
+            sec_display = 'seconds'
+
+        else:
+            sec_display = 'second'
+
+        # `timer` example: "Checking again in 1 second..."
+        timer = f'Checking again in {secs} {sec_display}...'
+
+        print(timer, end="\r")
+
+        # Wait a second
+        sleep(1)
+
+        # Take a second away from the total
+        seconds -= 1
+
+    clear_screen()
+
+
+if __name__ == '__main__':
+
+    pass
